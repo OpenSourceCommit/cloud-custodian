5c187ddd21904369d6dc7b6d8a73e0b206df406b
Usool-Data-Science <Adeshinaibrahim10@gmail.com>
2025-09-01 23:08:37 +0100

    Complete testing

--- c7n/resources/cw.py
+++ c7n/resources/cw.py
@@ -1324,6 +1324,7 @@ class SyntheticsCanary(QueryResourceManager):
         dimension = 'CanaryName'
         config_type = cfn_type = 'AWS::Synthetics::Canary'
         enum_spec = ('describe_canaries', 'Canaries', None)
+        detail_spec = ('get_canary', 'Name', 'Name' 'Canary')
         universal_taggable = True
 
     permissions = (
